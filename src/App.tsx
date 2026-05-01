4c74691468b2cad6558d81ce5853ce1815161a6d
LJ1319 <luka.jikia.2@btu.edu.ge>
2026-05-02 02:42:47 +0400

    break down components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
+import { Container } from "./components/Container";
+import { Header } from "./components/Header";
+import { NewPuppyForm } from "./components/NewPuppyForm";
+import { PageWrapper } from "./components/PageWrapper";
+import { PuppiesList } from "./components/PuppiesList";
+import { Search } from "./components/Search";
+import { Shortlist } from "./components/ShortList";
+
 function App() {
-  return <h1>Dev Pups</h1>;
+  return (
+    <PageWrapper>
+      <Container>
+        <Header />
+        <Main />
+      </Container>
+    </PageWrapper>
+  );
+}
+
+function Main() {
+  return (
+    <main>
+      <div className="mt-24 grid gap-8 sm:grid-cols-2">
+        <Search />
+        <Shortlist />
+      </div>
+      <PuppiesList />
+      <NewPuppyForm />
+    </main>
+  );
 }
 
 export default App;
